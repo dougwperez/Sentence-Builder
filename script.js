e980eec8961d31cee0748289acdc2ed3a9a17be0
Doug Perez <56580725+dougwperez@users.noreply.github.com>
2020-06-25 23:25:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,78 @@
-function startTimer(duration, display) {
-  var timer = duration,
-    minutes,
-    seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-    }
-  }, 1000);
-
-  if (timer == 4) {
-    console.log("yess");
-  }
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
-window.onload = function () {
-  var fiveMinutes = 5,
-    display = document.querySelector(".time");
-  startTimer(fiveMinutes, display);
-};
+.gameOverModal {
+  display: none;
+  line-height: 290%;
+  text-align: justify;
+  position: relative;
+  overflow-y: auto;
 
-function gameover() {
-  gameOverModal.style.display = "block";
-  console.log("Game Over");
+  max-height: 800px;
+  top: -20%;
+  left: 50%;
+  transform: translate(-50%, 50%);
+  width: 100%;
+  max-width: 45%;
+  height: 40%;
+  padding: 1.5em;
+  border: 2px solid black;
+
+  background-image: -webkit-linear-gradient(bottom, #f4e2c9 20%, #f4d7c9 100%);
+  background-image: -ms-linear-gradient(bottom, #f4e2c9 20%, #f4d7c9 100%);
+  background-image: linear-gradient(to bottom, #f4e2c9 20%, #f4d7c9 100%);
 }
 
-setTimeout(gameover, 5 * 60 * 1000);
+.closebtn {
+  font-size: 30px;
+  padding: 0.2em 0.4em 0.2em 0.4em;
+  float: right;
+  font-family: arial;
+  background-color: #f90b6d;
+  color: white;
+  cursor: pointer;
+  opacity: 0.6;
+  transition: 0.3s;
+}
+.closebtn:hover {
+  opacity: 1;
+}
+
+.viewer {
+  list-style-type: none;
+  flex: auto;
+  width: 249px;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  font-size: 1.05em;
+
+  font-family: sans-serif;
+  background-color: lightgrey;
+  border: 2px inset;
+
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  cursor: pointer;
+}
+
+.span {
+  float: left;
+  display: inline;
+}
+
+.span a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 13px;
+  text-decoration: none;
+}
+
+.span a:hover {
+  background-color: #f90b6d;
+}
