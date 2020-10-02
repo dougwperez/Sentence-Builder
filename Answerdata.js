bafb1b30dbd6aa2aac6c329d260df681ea9c82a2
Doug Perez <56580725+dougwperez@users.noreply.github.com>
2020-10-02 16:42:13 -0700

    Update Answerdata.js

--- Answerdata.js
+++ Answerdata.js
@@ -153,7 +153,7 @@ var finalAnswer = [
   "Helovesthedogandthecow",
 
   //randomnum 2
-  "dancingelephasfddfnt",
+  "dancingelephjjasfddfnt",
 "Theboylovesplayingspordtsatschool."
   
   "Sample sentence goefsasddcxfs here."
