ecfd30c35a410c159ecfb9ccb1b09cf78e567b2a
Doug Perez <56580725+dougwperez@users.noreply.github.com>
2020-10-03 18:02:53 -0700

    Update Answerdata.js

--- Answerdata.js
+++ Answerdata.js
@@ -154,7 +154,7 @@ var finalAnswer = [
 
   //randomnum 2
   "dancingelephjjasfddfnt",
-"Theboylovesplayingspordtsatschool."
+"Theboylovesplayingspodrdtsatschool."
   
   "Sample sentence goefsasddcxfs here."
 ];
